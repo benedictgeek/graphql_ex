db3ac5bf68f6c52a4cdd26ef9f33ee05e9aebf95
Olushola <olushola251@gmail.com>
2022-09-10 15:22:17 +0100

    setting up root queries for server

--- services/server/index.js
+++ services/server/index.js
@@ -1,4 +1,48 @@
 "use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const graphql_1 = require("graphql");
+const express_graphql_1 = require("express-graphql");
+const app = (0, express_1.default)();
+const cars = [
+    { id: 1, brand: "Audi", ownerId: 1 },
+    { id: 2, brand: "Totota", ownerId: 1 },
+    { id: 3, brand: "Lambo", ownerId: 1 },
+    { id: 4, brand: "Ferarri", ownerId: 2 },
+    { id: 5, brand: "BMW", ownerId: 2 },
+    { id: 6, brand: "Susuki", ownerId: 2 },
+    { id: 7, brand: "Volkswagen", ownerId: 3 },
+    { id: 8, brand: "Honda", ownerId: 3 },
+];
+const CarType = new graphql_1.GraphQLObjectType({
+    name: "Car",
+    description: "This is a car object",
+    fields: () => ({
+        id: { type: (0, graphql_1.GraphQLNonNull)(graphql_1.GraphQLInt) },
+        brand: { type: (0, graphql_1.GraphQLNonNull)(graphql_1.GraphQLString) },
+        ownerId: { type: (0, graphql_1.GraphQLNonNull)(graphql_1.GraphQLInt) },
+    }),
+});
+const AppQueryType = new graphql_1.GraphQLObjectType({
+    name: "Queryies",
+    description: "All queries",
+    fields: () => {
+        return {
+            cars: {
+                type: CarType,
+                description: "List all cars",
+                resolve: () => {
+                    return cars;
+                },
+            },
+        };
+    },
+});
+const Schema = new graphql_1.GraphQLSchema({
+    query: AppQueryType,
+    //  mutation: ""
+});
+app.use("/graphql", (0, express_graphql_1.graphqlHTTP)({ schema: Schema, graphiql: true }));
--- services/server/index.ts
+++ services/server/index.ts
@@ -1,5 +1,59 @@
 import express, { Request, Response, Express } from "express";
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLFieldConfig,
+  GraphQLFieldConfigMap,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLString,
+} from "graphql";
+import { graphqlHTTP } from "express-graphql";
 
 const app: Express = express();
 
+const cars = [
+  { id: 1, brand: "Audi", ownerId: 1 },
+  { id: 2, brand: "Totota", ownerId: 1 },
+  { id: 3, brand: "Lambo", ownerId: 1 },
+  { id: 4, brand: "Ferarri", ownerId: 2 },
+  { id: 5, brand: "BMW", ownerId: 2 },
+  { id: 6, brand: "Susuki", ownerId: 2 },
+  { id: 7, brand: "Volkswagen", ownerId: 3 },
+  { id: 8, brand: "Honda", ownerId: 3 },
+];
 
+const CarType = new GraphQLObjectType({
+  name: "Car",
+  description: "This is a car object",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    brand: { type: GraphQLNonNull(GraphQLString) },
+    ownerId: { type: GraphQLNonNull(GraphQLInt) },
+  }),
+});
+
+const AppQueryType = new GraphQLObjectType({
+  name: "Queryies",
+  description: "All queries",
+  fields: (): GraphQLFieldConfigMap<any, any> => {
+    return {
+      cars: {
+        type: CarType,
+        description: "List all cars",
+        resolve: () => {
+          return cars;
+        },
+      },
+    };
+  },
+});
+
+const Schema = new GraphQLSchema({
+  query: AppQueryType,
+  //  mutation: ""
+});
+
+app.use("/graphql", graphqlHTTP({ schema: Schema, graphiql: true }));
+
+app.listen(4040, () => console.log("Server started"));
