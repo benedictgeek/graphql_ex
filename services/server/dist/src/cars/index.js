c330d1a168c03dc4954277da150f60fda0318d5a
Olushola <olushola251@gmail.com>
2022-09-11 15:49:25 +0100

    wip

--- services/server/dist/src/cars/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.carsQuery = void 0;
-const queries_1 = require("./queries");
-Object.defineProperty(exports, "carsQuery", { enumerable: true, get: function () { return queries_1.carsQuery; } });
